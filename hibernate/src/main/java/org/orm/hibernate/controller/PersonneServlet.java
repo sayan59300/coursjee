7bd912fa7365e1bf045bf95cba5a81b3cc9c0c80
Buffart Nicolas <concepteur-developpeur@nicolas-buffart.fr>
2022-02-22 11:57:53 +0100

    projet fonctionnel

---
+++ hibernate/src/main/java/org/orm/hibernate/controller/PersonneServlet.java
@@ -0,0 +1,43 @@
+package org.orm.hibernate.controller;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.orm.hibernate.model.Personne;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "personne", value = "/personne")
+public class PersonneServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Personne personne = new Personne();
+
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+
+        personne.setNom(nom);
+        personne.setPrenom(prenom);
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(personne);//insert into
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
---
+++ hibernate/target/hibernate-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+<c:url value="/personne" var="lien_personne"/>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-4">
+            <h1>Personne</h1>
+            <a href="${lien_personne}">Personne</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
