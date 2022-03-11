d21a40f0720a1feb81afd79df9be9d6b0ebb5881
Buffart Nicolas <concepteur-developpeur@nicolas-buffart.fr>
2022-03-11 10:57:01 +0100

    suppression ancien projet

--- magasinSpring/src/main/java/com/magasinspring/dao/UtilisateurRepository.java
+++
@@ -1,7 +0,0 @@
-package com.magasinspring.dao;
-
-import com.magasinspring.model.Utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
-}
